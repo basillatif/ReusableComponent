e48a84c64287bdd1db325c72588d6389bdbdcf2c
basillatif <basillatif@gmail.com>
2017-12-15 11:02:10 -0800

    Removed extra code

--- plugin-demo.js
+++ plugin-demo.js
@@ -4,29 +4,7 @@ $(() => {
         $log.text($log.text() + message + "\n")
             .scrollTop($log[0].scrollHeight - $log.height());
     };
-
-    $(".edit-in-place").inPlaceEditor({
-        change: function () {
-            logEvent("In-place editor: " + $(this).text());
-        }
-    });
-
-    $("#demo-select").dropdownSelect({
-        initial: "Catch the",
-
-        options: [
-            "Catch the",
-            "tiger",
-            "by the",
-            $.fn.dropdownSelect.DIVIDER,
-            "toe"
-        ],
-
-        change: function (oldSelection, newSelection) {
-            logEvent("Dropdown select: Changed from '" + oldSelection + "' to '" + newSelection + "'");
-        }
-    });
-
+    
     $(".swivel-this").swivel({
         change: function (oldAngle, newAngle) {
             logEvent("Swivel: Swiveled from " + oldAngle + " to " + newAngle);
